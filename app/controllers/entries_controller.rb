6589b5b6ff598a079b0d207f275c9f8dc847f96d
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-03-02 06:06:52 +0000

    321206

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,16 @@ class EntriesController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session["user_id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
-
+end
 end
