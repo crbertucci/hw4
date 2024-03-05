565ba2416c563fa8bd1be307e7825e34a255ee7b
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-03-05 20:41:30 +0000

    351441

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,8 +8,9 @@ class EntriesController < ApplicationController
     if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
+    @entry["description"] = params["description"]
     @entry["place_id"] = params["place_id"]
     @entry["user_id"] = session["user_id"]
     @entry.save
